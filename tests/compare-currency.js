57bd71614a0c3632aede5d61c56341f9156380b3
sivakumar.kamichetty@amdocs.com <sivakumar.kamichetty@amdocs.com>
2021-02-18 03:49:28 +0800

    Signed-off-by: sivakumar.kamichetty@amdocs.com <sivakumar.kamichetty@amdocs.com>

--- tests/compare-currency.js
+++ tests/compare-currency.js
@@ -1,4 +1,4 @@
-const request = require('request');
+const rp = require('request-promise');  
 let outputFromUI = 0;
 let outputFromAPI = 0;
 browser.waitForAngularEnabled(false);
@@ -6,11 +6,10 @@ describe ('Currency Comparision Test', function(){
     it('USD value comparision between API and UI', function(){
         browser.waitForAngularEnabled(false);
         browser.get('https://www.ecb.europa.eu/stats/policy_and_exchange_rates/euro_reference_exchange_rates/html/index.en.html');  
-        
         let result = element(by.css('[href="eurofxref-graph-usd.en.html"] > .rate')).getText().then(function (outputFromUI) {
         console.log('output From UI =' + outputFromUI);
        });        
-        request('https://api.exchangeratesapi.io/latest', function (error, response, body) {
+       rp('https://api.exchangeratesapi.io/latest', function (error, response, body) {
             const obj = JSON.parse(body);
             outputFromAPI = obj.rates.USD;  
             console.log('output From API =' + outputFromAPI); 
